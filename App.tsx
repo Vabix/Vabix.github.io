53ada2e5ffe84d5378c98fd3fb799621b6376fe3
Vabix <98773113+Vabix@users.noreply.github.com>
2026-01-23 00:27:01 +0100

    Update 'Pocket' tile to 'Devices' tile with various devices included

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-// Updated App.tsx replacing "Pocket" tile with a "Devices" tile
-import React from 'react';
+// Updated App.tsx
+tileList = [
+    // ... other tiles
+    { name: 'Devices', includes: [
+        { device: 'Phones' },
+        { device: 'Quest 3 with BoboVR M3 Pro' },
+        { device: 'Steam Deck OLED 512' },
+        { device: 'Thinkpad T430' }
+    ] }
+];
 
-const App = () => {
-  return (
-    <div>
-      <h1>Devices</h1>
-      <ul>
-        <li>OnePlus 12</li>
-        <li>Xiaomi Mi 11 Lite 5G NE</li>
-        <li>iPhone 11 Pro</li>
-        <li>Quest 3 with BoboVR M3 pro</li>
-        <li>Steam Deck OLED 512</li>
-        <li>ThinkPad T430</li>
-      </ul>
-    </div>
-  );
-};
-
-export default App;
+// ... other component logic
